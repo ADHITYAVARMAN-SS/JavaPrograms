6410374ef507fcd7f70bd10069d176e9f5414f83
Adhityavarman s s <103426478+ADHITYAVARMAN-SS@users.noreply.github.com>
2023-07-22 08:52:12 +0530

    Add files via upload

---
+++ OddOrEvenCount.java
@@ -0,0 +1,45 @@
+package Training;
+
+import java.util.Scanner;
+
+public class OddOrEvenCount {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		
+		char arr[] = a.toCharArray();
+		int len = arr.length,even = 0, odd = 0;
+		char even1[] = new char[len],odd1[] = new char[len];int j = 0,k = 0;
+		
+		for(int i = 0;i< len;i++) {
+			if(arr[i]%2 == 0) {
+				even1[j]=arr[i];
+				j++;
+				even++;
+				continue;
+		}
+			odd1[k]=arr[i];
+			k++;
+			odd++;
+		}
+		System.out.println("Even : "+even+" Odd : "+odd);
+		
+		System.out.println("Even digits");
+		for(int i = 0;i < even1.length;i++) {
+			if(even1[i]==0) {
+				continue;
+			}
+			System.out.print(even1[i]+" ");
+		}
+		System.out.println("\n\nOdd digits");
+		for(int i = 0;i < even1.length;i++) {
+			if(odd1[i]==0) {
+				continue;
+			}
+			System.out.print(odd1[i]+" ");
+		}
+		
+//		need to find prime numbers count
+	}
+}
